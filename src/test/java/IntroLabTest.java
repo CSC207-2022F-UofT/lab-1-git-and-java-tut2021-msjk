df3dbae8b2f36484ff804a22547c53282c7a7ff3
Sharon Hsieh <sharonyt22@gmail.com>
2022-09-12 13:36:58 -0400

    Added method hsiehsh7

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -30,4 +30,11 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 1000)
+    public void testMethod() {
+        String expected = "Hi! My name is Sharon! " +
+                "I like cats more than dogs! Welcome to CSC207!";
+        String actual = IntroLab.hsiehsh7();
+        assertEquals(expected, actual);
+    }
 }
